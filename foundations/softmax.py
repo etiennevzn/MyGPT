558418220a8f851c4c561da96f9e220f718bd4ef
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-03 19:31:35 +0200

    Update Softmax function (Math Foundations 3/6)

--- foundations/softmax.py
+++ foundations/softmax.py
@@ -7,8 +7,5 @@ class Solution:
     def softmax(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         newZ = z - np.max(z)
         expZ = np.exp(newZ)
-        sumZ = np.sum(expZ)
-        expZ /= sumZ
-
-        return np.round(expZ,4)
+        return np.round(expZ / np.sum(expZ),4)
 
